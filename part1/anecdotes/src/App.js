acbc660e846553d4383c4f06c1549682579d2c2c
Andrew Ooi <andrewooi@Andrews-MacBook-Air.local>
2023-08-14 00:43:40 +0800

    Destructured a parameter

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -1,8 +1,8 @@
 import { useState } from 'react'
 
-const Button = (props) => (
-  <button onClick={props.handleClick}>
-    {props.text}
+const Button = ({handleClick, text}) => (
+  <button onClick={handleClick}>
+    {text}
   </button>
 )
 
